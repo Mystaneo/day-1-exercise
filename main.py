38f58e41fb814caa3d72cf0f3576691c059d250d
kaduell <92281981+Mystaneo@users.noreply.github.com>
2021-10-11 03:09:32 +0000

    input + comments

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-print("Hello" + " " + "Angela")
-
-
-
-
-
-
-
+# input() will get user input in compile
+# Then print () will print the word "Hello" and the user input
+print("Hello " + input("What is your name?"))
